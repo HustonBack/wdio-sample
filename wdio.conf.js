bd7106c80f7cc894e7c2157c540597f27f029206
HustonBack <ivan.samiliak@gmail.com>
2019-08-13 11:02:13 +0300

    fix headless

--- wdio.conf.js
+++ wdio.conf.js
@@ -52,12 +52,16 @@ exports.config = {
         // maxInstances can get overwritten per capability. So if you have an in-house Selenium
         // grid with only 5 firefox instances available you can make sure that not more than
         // 5 instances get started at a time.
-        maxInstances: 5,
+        maxInstances: 1,
         //
         browserName: 'chrome',
+
+        'goog:chromeOptions': {
+            args: ['--headless', '--disable-gpu', '--no-sandbox'],
+        },
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
-        excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
+        // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
         // excludeDriverLogs: ['bugreport', 'server'],
     }],
     //
